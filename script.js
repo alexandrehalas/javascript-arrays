8ddbfb55cee7c39be0e526404104dcae083dd02a
Alexandre Halas <alexandrehalas@gmail.com>
2023-08-24 20:49:30 -0300

    Reduce method

--- script.js
+++ script.js
@@ -82,14 +82,18 @@ const displayMovements = function (movements) {
   });
 };
 
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111,
-// };
+const calculateBalance = function (movements) {
+  return movements.reduce((accumulated, movement) => accumulated + movement, 0);
+};
+
+const displayBalance = function (movements) {
+  const balance = calculateBalance(movements);
+  labelBalance.textContent = `${balance} EUR`;
+};
 
 displayMovements(account1.movements);
+
+displayBalance(account1.movements);
 
 const createUsersNames = function (accounts) {
   return accounts.forEach(
@@ -106,7 +110,6 @@ const userName = createUsersNames(accounts);
 
 console.log(accounts);
 
-/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
@@ -324,3 +327,27 @@ console.log(depositsFor);
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
 */
+
+// THE REDUCE METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
+console.log(balance);
+
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
+
+// Maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
