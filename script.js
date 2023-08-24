f8ac97016b4efd1f041b953aa7cd1fe5eca7ae66
Alexandre Halas <alexandrehalas@gmail.com>
2023-08-24 15:50:20 -0300

    Map method

--- script.js
+++ script.js
@@ -242,7 +242,7 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
-
+/*
 const juliaDogsAge = [3, 5, 2, 12, 7];
 const kateDogsAge = [4, 1, 15, 8, 3];
 
@@ -264,3 +264,30 @@ const checkDogs = function (dogsJulia, dogsKate) {
 };
 
 checkDogs(juliaDogsAge, kateDogsAge);
+*/
+
+// THE MAP METHOD
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDescriptions);
