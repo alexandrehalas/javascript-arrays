f15100b3cb1982a5c3eb188a79b6e9f65a39109a
Alexandre Halas <alexandrehalas@gmail.com>
2023-08-26 11:02:21 -0300

    Code Challenge #3

--- script.js
+++ script.js
@@ -400,7 +400,7 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
-
+/*
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(dogAge =>
     dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4
@@ -410,6 +410,34 @@ const calcAverageHumanAge = function (ages) {
     adults.reduce((accumulator, age) => accumulator + age, 0) / adults.length;
   return average;
 };
+
+const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+const average2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(average1);
+console.log(average2);
+*/
+
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+    .filter(humanAge => humanAge >= 18)
+    .reduce(
+      (accumulator, age, index, array) => accumulator + age / array.length,
+      0
+    );
 
 const average1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 
