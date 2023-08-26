e3bab3c797f63ecba44d8d8f8c86aef427510af8
Alexandre Halas <alexandrehalas@gmail.com>
2023-08-26 16:44:00 -0300

    flat and flatMap methods

--- script.js
+++ script.js
@@ -540,6 +540,7 @@ const jessicaAccount = accounts.find(
 console.log(jessicaAccount);
 */
 
+/*
 // SOME AND EVERY
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -563,3 +564,24 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+// FLAT AND FLATMAP
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
